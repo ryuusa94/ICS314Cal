fa702aa20952a31d0f7369a5718b841480e04ad4
ryuusa94 <ryuusa94@live.com>
2015-06-30 20:26:38 -1000

    updated CalObj by Adam
    updated the template file chad made with the class file we came up with together as a group in the library

--- ICS314Cal/src/CalObj.java
+++ ICS314Cal/src/CalObj.java
@@ -10,11 +10,17 @@
  */
 public class CalObj 
 {
+	private String name;
+  	private String time;
+	private String timezone;
+	private String date;
+	private String[] location;
+	private String coordinates;
+  	private String summary;
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Some words.");
 	}
 	//comment added for testing purposes.
-
 }
