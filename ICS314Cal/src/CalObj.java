69175bb3d2fb55cfa6e54dd63271dedc886424c2
Chad Delp <cdelp@hawaii.edu>
2015-07-01 18:06:27 -1000

    Fetched

--- ICS314Cal/src/CalObj.java
+++ ICS314Cal/src/CalObj.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 
 /**
  * empty class, using to test github setup
@@ -10,17 +8,11 @@
  */
 public class CalObj 
 {
-	private String name;
-  	private String time;
-	private String timezone;
-	private String date;
-	private String[] location;
-	private String coordinates;
-  	private String summary;
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Some words.");
 	}
 	//comment added for testing purposes.
+
 }
