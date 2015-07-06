41d7150f42eea7d9da58b06602f0af4b7bf2c745
Chad Delp <cdelp@hawaii.edu>
2015-07-06 00:13:47 -1000

    Replaced CalObj with Jarrod's code.

--- ICS314Cal/src/CalAssignment/CalObj.java
+++ ICS314Cal/src/CalAssignment/CalObj.java
@@ -1,166 +1,127 @@
 package CalAssignment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 /**
  * CalObj class
- * private variables, empty constructor, public setters and getters,
- * and MAC CALENDAR toString
+ * 
  */
 public class CalObj 
 {
-	private String name; //associated with SUMMARY category
-  	private String startTime; 
-  	private String endTime; 
-  	private String timezone; //will be Pacific/Honolulu here
-	private String date; //may need to mix date and time, exp: 201500701T010000
+	
+	private String version =  	"VERSION:1.0\r\n";
+	private String prodid = 	"PRODID://Elara/lofy/tanare/delp/314sum2015//\r\n";
+	private String calBegin =  	"BEGIN:VCALENDAR\r\n";
+	private String calEnd =  	"END:VCALENDAR\r\n";
+	private String eventBegin = "BEGIN:VEVENT\r\n";
+	private String eventEnd =   "END:VEVENT\r\n";
+	
+	private String eventSummary;
+	private String eventStartTime;
+	private String eventEndTime;
+	private String eventUid;
+	private String eventTimezone;
+	private String eventGeoPos;
+	private String eventClass;
+		
+	//kept these from eariler draft
 	private String[] location;
 	private Float lat;//latitude used for geographic position
 	private Float lon;//longitude used for geographic position
-	private String coordinates; //could lump lat and long into string if needed
-  	private String descript; //associated with DESCRIPTION category
-  	  	
-  	public CalObj()
-  	{
-  		
-  	}
-  	
-	public String getName() 
-	{
-		return name;
-	}
-	public void setName(String name) 
-	{
-		this.name = name;
-	}
-	public String getStartTime() 
-	{
-		return startTime;
-	}
-	public void setStartTime(String startTime) 
-	{
-		this.startTime = startTime;
-	}
-	public String getEndTime() 
-	{
-		return endTime;
-	}
-	public void setEndTime(String endTime) 
-	{
-		this.endTime = endTime;
-	}
-	public String getTimezone() 
-	{
-		return timezone;
-	}
-	public void setTimezone(String timezone) 
-	{
-		this.timezone = timezone;
-	}
-	public String getDate() 
-	{
-		return date;
-	}
-	public void setDate(String date) 
-	{
-		this.date = date;
-	}
-	public String[] getLocation() 
-	{
-		return location;
-	}
-	public void setLocation(String[] location) 
-	{
-		this.location = location;
-	}
-	public String getCoordinates() 
-	{
-		return coordinates;
-	}
-	public Float getLat() {
-		return lat;
-	}
-
-	public void setLat(Float lat) {
-		this.lat = lat;
-	}
-
-	public Float getLon() {
-		return lon;
-	}
-
-	public void setLon(Float lon) {
-		this.lon = lon;
-	}
-
-	public void setCoordinates(String coordinates) 
-	{
-		this.coordinates = coordinates;
-	}
-	public String getDescript() 
-	{
-		return descript;
-	}
-	public void setDescript(String descript) 
-	{
-		this.descript = descript;
+	
+	
+	public void setSummary( String summary){
+		StringBuilder builder = new StringBuilder();
+		builder.append("SUMMARY:");
+		builder.append(summary);
+		builder.append("\r\n");
+		eventSummary = builder.toString();
+		
+		
 	}
 	
-	/* toString method
-	 * WORKS WITH MAC CALENDAR
-	 * several of the lines appended below with need to be changed from
-	 * hard coding to variables 
-	 */
-/*	public String toString()
-	{
-		return new StringBuffer("BEGIN:VCALENDAR\n")
-			.append("CALSCALE:GREGORIAN\n")
-			.append("VERSION:2.0\n")
-			.append("METHOD:PUBLISH\n")			
-			.append("X-WR-CALNAME:Home\n")
-			.append("X-WR-TIMEZONE:"+ this.timezone +"\n")
-			.append("DTSTART:19330430T020000\n")//don't know why this date is so old
-			.append("X-APPLE-CALENDAR-COLOR:#1BADF8\n")
-			.append("BEGIN:VEVENT\n")
-			.append("UID:6BB808D7-C47A-4BDC-AF61-63B65AC28D0C\n")
-			.append("DTEND;TZID="+ this.timezone +":"+ this.endTime +"\n") //need to mix date with time?
-			.append("TRANSP:OPAQUE\n")
-			.append("CREATED:20150701T052850Z\n")
-			.append("SUMMARY:" + this.name + "\n")
-			.append("DTSTART;TZID="+ this.timezone +":" + this.startTime + "\n") //need to mix date with time?
-			.append("DTSTAMP:20150701T090000Z\n")
-			.append("LOCATION:" + this.location + "\n")
-			.append("GEO:" + this.lat + ";" + this.lon + "\n")//geographic position
-			.append("SEQUENCE:0\n")
-			//.append("SEQUENCE:" + Driver.sequence + "\n")//don't know if sequence needs to increase
-			.append("END:VEVENT\n")
-			.append("END:VCALENDAR\n").toString();
+	public void setStart( String time){
+		StringBuilder builder = new StringBuilder();
+		builder.append("DTSTART:");
+		builder.append(time);
+		builder.append("\r\n");
+		eventStartTime = builder.toString();
+		
 	}
-*/
 	
-	/* toString method
-	 * WORKS WITH GOOGLE CALENDAR (also works with Mac, but not as well as Mac version)
-	 * Commented lines are still untested, and are likely unneeded.
-	 */
-	public String toString()
-	{
-		return new StringBuffer("BEGIN:VCALENDAR\n")			
-			//.append("X-WR-TIMEZONE:"+ this.timezone +"\n")
-			.append("BEGIN:VEVENT\n")
-			.append("DTSTART;VALUE=DATE:" + this.date + "\n")
-			.append("DTEND;VALUE=DATE:20150705\n") //need end date variable
-			//.append("DTSTAMP:20150701T090000Z\n")
-			//.append("UID:0pcbu9mp1eqeak59v0m04lspn0@google.com\n") //don't know how to use this
-			//.append("CREATED:20150701T052850Z\n")
-			.append("DESCRIPTION:" + this.descript + "\n")
-			//.append("LAST-MODIFIED:20150701T052850Z\n")
-			.append("LOCATION:" + this.location + "\n")
-			.append("GEO:" + this.lat + ";" + this.lon + "\n")//geographic position
-			//.append("SEQUENCE:0\n")
-			//.append("STATUS:CONFIRMED\n")
-			.append("SUMMARY:" + this.name + "\n")
-			//.append("TRANSP:TRANSPARENT\n")			
-			.append("END:VEVENT\n")
-			.append("END:VCALENDAR\n").toString();
+	public void setEnd(String time){
+		StringBuilder builder = new StringBuilder();
+		builder.append("DTEND:");
+		builder.append(time);
+		builder.append("\r\n");
+		eventEndTime = builder.toString();
+		
 	}
+	
+	public void setUID( String uid){
+		StringBuilder builder = new StringBuilder();
+		builder.append("UID:");
+		builder.append(uid);
+		builder.append("\r\n");
+		eventUid = builder.toString();
+		
+	}
+	
+	public void setTimeZone(String tz){
+		
+	}
+	
+	public void setGeographicPosition(String geoPos){
+		
+	}
+	
+	public void setClassification(String cla ){
+		
+	}
+	
 
-	
+public void write( String name ){
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append(name);
+		builder.append(".ics");
+		
+		try {
+			
+ 
+			File file = new File(builder.toString());
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(calBegin);
+			bw.write(version);
+			bw.write(prodid);
+			bw.write(eventBegin);
+			bw.write(eventUid);
+			bw.write(eventStartTime);
+			bw.write(eventEndTime);
+			bw.write(eventSummary);
+			bw.write(eventEnd);
+			bw.write(calEnd);
+			
+			
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
--- ICS314Cal/src/CalAssignment/Driver.java
+++ ICS314Cal/src/CalAssignment/Driver.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 
 /**
  * Driver Class 
- * Making a CalObj and writing toString to file for testing purposes.
- * NOT FINAL CODE. Please change or refactor as needed.
+ * NOT FINAL CODE
+ * Creating CalObj for testing
  *
  */
 public class Driver {
@@ -16,40 +16,11 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		CalObj event1 = new CalObj();
-		event1.setName("Fireworks");
-		event1.setStartTime("20150704T210000");
-		event1.setEndTime("20150704T220000");
-		event1.setDate("20150704");
-		event1.setTimezone("Pacific/Honolulu");
-		event1.setDescript("Watching fireworks at Ala Moana.");
-		event1.setLat((float) 21.285247);
-		event1.setLon((float) -157.845855);
+		event1.setSummary("test summary");
+		event1.setUID("testEventID");
+		event1.setStart("9:00");
+		event1.setEnd("10:00");
+		event1.write("test2");
 		
-		try
-		{
-			//creating file
-			File writeF = new File("testCalGOOGLE.ics");
-			
-			if (!writeF.exists())
-			{
-				writeF.createNewFile();
-			}
-			
-			//setting up write buffer
-			FileWriter fw = new FileWriter(writeF.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			
-			//writing event1
-			bw.write(event1.toString());
-			
-			bw.close();
-			
-			System.out.println("file written");
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
 	}
-
 }
