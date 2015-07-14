b1c915aa4be1b18a20f4915ba28bfd66b6b81f89
cdelp <cdelp@hawaii.edu>
2015-07-13 20:17:02 -1000

    Added setEnd tests.

--- ICS314Cal/src/CalAssignment/JUnit_Event.java
+++ ICS314Cal/src/CalAssignment/JUnit_Event.java
@@ -24,6 +24,18 @@ public class JUnit_Event {
 	}
 	
 	/**
+	 * testing setEnd called with time zone info during file writing
+	 */
+	@Test
+	public void setEndTZtest() {
+		Event testEvent = new Event();
+		//testing with time zone
+		Interface.setTzInfo(true);
+		testEvent.setEnd("20150710", "1500", "Pacific");
+		assertEquals("DTEND;TZID=Pacific:20150710T150000\r\n" , testEvent.getEventEndTime());		
+	}
+	
+	/**
 	 * testing setStart called without time zone info during file writing
 	 */
 	@Test
@@ -36,14 +48,59 @@ public class JUnit_Event {
 	}
 	
 	/**
+	 * testing setEnd called without time zone info during file writing
+	 */
+	@Test
+	public void setEndNoTZtest() {
+		Event testEvent2 = new Event();
+		//testing without time zone
+		Interface.setTzInfo(false);
+		testEvent2.setEnd("20150710", "1500", null);
+		assertEquals("DTEND:20150710T150000\r\n" , testEvent2.getEventEndTime());		
+	}
+	
+	/**
 	 * Testing setStart called during file reading
 	 */
 	@Test
-	public void setStartReadTest() {
+	public void setStartTZReadTest() {
 		Event testEvent3 = new Event();
 		//testing with time zone		
 		testEvent3.setStart("TZID=Pacific:20150710T150000");
 		assertEquals("DTSTART;TZID=Pacific:20150710T150000\r\n" , testEvent3.getEventStartTime());		
+	}
+	
+	/**
+	 * Testing setEnd called during file reading
+	 */
+	@Test
+	public void setEndTZReadTest() {
+		Event testEvent3 = new Event();
+		//testing with time zone		
+		testEvent3.setEnd("TZID=Pacific:20150710T150000");
+		assertEquals("DTEND;TZID=Pacific:20150710T150000\r\n" , testEvent3.getEventEndTime());		
+	}
+	
+	/**
+	 * Testing setStart called during file reading
+	 */
+	@Test
+	public void setStartNoTZReadTest() {
+		Event testEvent4 = new Event();
+		//testing without time zone		
+		testEvent4.setStart("20150710T150000");
+		assertEquals("DTSTART:20150710T150000\r\n" , testEvent4.getEventStartTime());		
+	}
+	
+	/**
+	 * Testing setEnd called during file reading
+	 */
+	@Test
+	public void setEndNoTZReadTest() {
+		Event testEvent4 = new Event();
+		//testing without time zone		
+		testEvent4.setEnd("20150710T150000");
+		assertEquals("DTEND:20150710T150000\r\n" , testEvent4.getEventEndTime());		
 	}
 
 }
